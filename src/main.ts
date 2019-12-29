620036c67124387d61be6790d635b066913ded13
nagappan <nagappan08@gmail.com>
2019-12-29 23:49:35 +0530

    Initial Commit

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, dialog, ipcMain, IpcMain, IpcMainEvent } from "electron";
+import * as fs from "fs";
 import * as path from "path";
 
 let mainWindow: Electron.BrowserWindow;
@@ -6,15 +7,49 @@ let mainWindow: Electron.BrowserWindow;
 function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    height: 600,
+    fullscreen: true,
     webPreferences: {
+      nodeIntegration: true,
       preload: path.join(__dirname, "preload.js"),
     },
-    width: 800,
   });
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, "../index.html"));
+
+  mainWindow.webContents.on("new-window", (event: Event, url, frameName, disposition, options, additionalFeatures) => {
+    if (frameName === "modal") {
+      // open window as modal
+      Object.assign(options, {
+        modal: true,
+        parent: mainWindow,
+        width: 300,
+        titleBarStyle: "hidden",
+        frame: true,
+        height: 100
+      });
+      return new BrowserWindow(options);
+    }
+  })
+
+  ipcMain.on("showFolderDialog", (event: IpcMainEvent) => {
+    let fileSelectionPromise = dialog.showOpenDialog({properties: ["openFile", "openDirectory", "multiSelections"]});
+    fileSelectionPromise.then(function(obj) {
+        event.sender.send("selectedfolders", obj.filePaths);
+        let cumfileslist = obj.filePaths.map((filePath, index)=>{
+          return fs.readdirSync(filePath, {withFileTypes: true})
+                   .filter(dirent=>!dirent.isDirectory())
+                   .map(dirent=>filePath + "/" + dirent.name);
+        }).reduce((filesacc, files) => {
+            filesacc = filesacc.concat(files);
+            return filesacc;
+        }).every((absolutefilepath, index, array) => {
+          let stats:fs.Stats = fs.statSync(absolutefilepath);
+          event.sender.send("fileslist", path.basename(absolutefilepath), stats);
+          return true;
+        });
+    });
+  });
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
--- src/renderer.ts
+++ src/renderer.ts
@@ -1,3 +1,72 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+import { ipcRenderer, IpcRendererEvent } from "electron";
+import * as fs from "fs";
+
+window.addEventListener("DOMContentLoaded", () => {
+    const btn: HTMLElement = document.getElementById("folderchooser");
+
+    btn.addEventListener("click", (ev: Event) => {
+        ipcRenderer.send("showFolderDialog");
+    });
+});
+
+document.getElementById("clearselection").addEventListener("click", () => {
+    const selectionField = document.getElementById("selectedfolders") as HTMLInputElement;
+    selectionField.value = "";
+    document.getElementsByTagName("ul")[0].innerHTML = "";
+});
+
+ipcRenderer.on("selectedfolders", (evt: IpcRendererEvent, selectedfolders: string[]) => {
+    const selectedFolderElem: HTMLInputElement = document.getElementById("selectedfolders") as HTMLInputElement;
+    selectedFolderElem.value = selectedFolderElem.value !== "" ? selectedFolderElem.value + "|"
+                                                            : selectedFolderElem.value ;
+    selectedFolderElem.value += selectedfolders.join(" | ");
+});
+
+ipcRenderer.on("fileslist", (event: IpcRendererEvent, fileName: string, stats: fs.Stats) => {
+    const filetemplate = document.getElementById("filerec") as HTMLTemplateElement;
+    const filedisplayrec = filetemplate.content;
+    const spanElements = filedisplayrec.querySelectorAll("span");
+    spanElements[0].innerText = fileName;
+    spanElements[1].innerText = stats.size.toString();
+    spanElements[2].innerText = stats.mtime.toString();
+    // spanElements[3].id = fileName + "_tags";
+    // const btnElements = filedisplayrec.querySelectorAll("button");
+
+    // btnElements[0].id = fileName;
+    const nodeElement: Node = filedisplayrec.cloneNode(true);
+
+    document.getElementById("filelist").appendChild(nodeElement);
+
+    // for (let tagElement of document.getElementsByClassName("addtag")) {
+    //     tagElement.addEventListener("click", addtaghandler);
+    // }
+});
+
+// let modal:Window;
+// let fileName: String;
+
+// function addtaghandler(evt: Event) {
+//     console.log("addtaghandler");
+//     console.log(evt);
+//     // renderer process (mainWindow)
+//     //fileName = input;
+//     modal = window.open("popuptagform.html", "modal");
+//     console.log(modal);
+// }
+
+// function canceltag() {
+//     alert("cancelled");
+//     modal.close();
+// }
+
+// function addtag() {
+//     alert("addtag");
+//     let newname = document.getElementById("newtagname").nodeValue;
+//     let labelElement = document.createElement("label");
+//     labelElement.innerText += newname;
+//     document.getElementById(fileName + "_tagname").appendChild(labelElement);
+// }
